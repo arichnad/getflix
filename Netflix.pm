2de01aaae60590da9f908485539c9644c7b6858e
adrian <adrian-sw@aporter.org>
2011-12-08 12:22:34 -0500

    updating the regular expression for 2011 again (minor whitespace change)

--- Netflix.pm
+++ Netflix.pm
@@ -75,7 +75,7 @@ sub getRatings {
 # This is the main Regular Expression. If Netflix ever changes their web site, 
 # this regular expression will need to change as well.
 #60000165~Erin Brockovich~2000~R~Drama~5
-		while ( $body =~ s/^.*?(?:movieid=(\d+)|unav).*?>.*? id="b[^"]*?".*?>([^<]+).*?"genre">(.*?)<.*?(?:you rated this movie: (\d)|Average rating)//gsi ) {
+		while ( $body =~ s/^.*?(?:movieid=(\d+)|unav).*?>.*? id="b[^"]*?".*?>\s*([^<]+?)\s*<.*?"genre">(.*?)<.*?(?:you rated this movie: (\d)|Average rating)//gsi ) {
 			print FD "$1~$2~~~$3~$4\n";
 		}
 		++$cur;
